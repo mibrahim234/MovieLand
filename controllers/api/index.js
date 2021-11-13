7d46bec92339b502ecb919e4bc9a1514be50a9b5
mibrahim234 <mohamm1417@gmail.com>
2021-11-13 17:47:44 -0500

    added some model

---
+++ controllers/api/index.js
@@ -0,0 +1,7 @@
+// collect all of the API routes and package them up for us.
+const router = require('express').Router();
+
+// const apiRoutes = require('./api/');
+// const homeRoutes = require('./home-routes.js');
+
+module.exports = router;
