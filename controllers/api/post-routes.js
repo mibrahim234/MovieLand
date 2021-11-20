71d01cb46d11e588ffa8065f89ad24da152309ce
mibrahim234 <mohamm1417@gmail.com>
2021-11-19 21:42:53 -0500

    pushing up comments,centered

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -78,7 +78,7 @@ router.get("/:id", (req, res) => {
         });
 });
 
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         review: req.body.review,
